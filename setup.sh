23fb35f03fdab08dfddbc8f7777626cb4c730079
MrZelly <jurko.minaric@gmail.com>
2025-09-01 17:22:39 +0200

    Fixes and improvements

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-echo "updating system"
+echo -e "\e[1m\e[32m ----Updating System---- \e[0m"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y git dfu-util
-cd ..
-echo "downloading custom klipper"
+cd
+echo -e "\e[1m\e[32m ----Downloading custom Klipper---- \e[0m"
 rm -rf klipper
 git clone https://github.com/Melt3dDev/klipper
 cd setup
-echo "copying klipper config, mainsail theme and plymouth theme"
+echo -e "\e[1m\e[32m ----Copying Klipper config, Mainsail theme and Plymouth theme---- \e[0m"
 rm /home/biqu/printer_data/config/printer.cfg
 cp printer.cfg /home/biqu/printer_data/config/
 cp can.cfg /home/biqu/printer_data/config/
@@ -17,9 +16,10 @@ sudo cp bgrt-fallback.png /usr/share/plymouth/themes/armbian/
 sudo rm /usr/share/plymouth/themes/armbian/watermark.png
 sudo cp watermark.png /usr/share/plymouth/themes/armbian/
 sudo rm /boot/armbiainEnv.txt
-sudo cp armbiainEnv.txt /boot/
+sudo cp armbianEnv.txt /boot/
 sudo rm /boot/system.cfg
 sudo cp system.cfg /boot/
+echo -e "\e[1m\e[32m ----Flashing Manta---- \e[0m"
 echo "Put Manta into boot mode"
 read -p "Press enter to start flashing katapult to Manta"
 sudo dfu-util -a 0 -D ~/setup/katapult_manta.bin --dfuse-address 0x08000000:force:leave -d 0483:df11
@@ -27,6 +27,7 @@ echo "Reset Manta, then put it into boot mode"
 read -p "Press enter to start flashing klipper to Manta"
 sudo dfu-util -a 0 -d 0483:df11 --dfuse-address 0x08020000 -D ~/setup/klipper_manta.bin
 echo "Reset Manta again"
+echo -e "\e[1m\e[32m ----Flashing EBB Can---- \e[0m"
 echo "Connect can via USB and put it into boot mode"
 read -p "Press enter to start flashing katapult to Can"
 sudo dfu-util -a 0 -D ~/setup/katapult_can.bin --dfuse-address 0x08000000:force:leave -d 0483:df11
@@ -34,7 +35,7 @@ echo "Reset Can, then put it into boot mode"
 sudo dfu-util -a 0 -d 0483:df11 --dfuse-address 0x08002000 -D ~/klipper_can.bin
 read -p "Disconnect Can USB then press enter"
 sudo ifup can0
-echo "Querying Manta UUID"
+echo -e "\e[1m\e[32m ----Querying Manta UUID---- \e[0m"
 manta_uuid_querry=( $(python3 ~/setup/katapult/scripts/flash_can.py -q) )
 manta_uuid=( $(echo ${manta_uuid_querry[11]::-1}) )
 echo Manta UUID: $manta_uuid
@@ -43,12 +44,13 @@ echo "Querying Can UUID"
 can_uuid_querry=( $(python3 ~/setup/katapult/scripts/flash_can.py -q) )
 can_uuid=( $(echo ${manta_uuid_querry[16]::-1}) )
 echo Manta UUID: $can_uuid
-echo "Setting manta uuid"
+echo -e "\e[1m\e[32m ----Setting manta uuid---- \e[0m"
 echo [mcu] >> /home/biqu/printer_data/config/printer.cfg
 echo canbus_uuid: $manta_uuid >> /home/biqu/printer_data/config/printer.cfg
 echo canbus_interface: can0 >> /home/biqu/printer_data/config/printer.cfg
-echo "Setting ebb can uuid"
+echo -e "\e[1m\e[32m ----Setting ebb can uuid---- \e[0m"
 echo [mcu EBBCan] >> /home/biqu/printer_data/config/can.cfg
 echo canbus_uuid: $can_uuid >> /home/biqu/printer_data/config/can.cfg
 echo canbus_interface: can0 >> /home/biqu/printer_data/config/can.cfg
+echo -e "\e[1m\e[32m ----Everything done---- \e[0m"
 

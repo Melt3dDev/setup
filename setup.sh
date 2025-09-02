0a658af47d6a4f266d90d779945338d2ca3cb0f0
MrZelly <jurko.minaric@gmail.com>
2025-09-02 17:17:55 +0200

    .

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo -e "\e[1m\e[32m ----Updating System---- \e[0m"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 cd
-echo -e "\e[1m\e[32m ----Downloading custom Klipper---- \e[0m"
+echo -e "\e[1m\e[32m ----Downloading modified Klipper---- \e[0m"
 rm -rf klipper
 git clone https://github.com/Melt3dDev/klipper
 cd setup
@@ -28,29 +28,31 @@ read -p "Press enter to start flashing klipper to Manta"
 sudo dfu-util -a 0 -d 0483:df11 --dfuse-address 0x08020000 -D ~/setup/klipper_manta.bin
 echo "Reset Manta again"
 echo -e "\e[1m\e[32m ----Flashing EBB Can---- \e[0m"
-echo "Connect can via USB and put it into boot mode"
-read -p "Press enter to start flashing katapult to Can"
+echo "Connect EBB Can via USB and put it into boot mode"
+read -p "Press enter to start flashing katapult to EBB Can"
 sudo dfu-util -a 0 -D ~/setup/katapult_can.bin --dfuse-address 0x08000000:force:leave -d 0483:df11
-echo "Reset Can, then put it into boot mode"
+echo "Reset EBB Can, then put it into boot mode"
 sudo dfu-util -a 0 -d 0483:df11 --dfuse-address 0x08002000 -D ~/klipper_can.bin
-read -p "Disconnect Can USB then press enter"
+read -p "Disconnect EBB Can USB then press enter"
 sudo ifup can0
 echo -e "\e[1m\e[32m ----Querying Manta UUID---- \e[0m"
 manta_uuid_querry=( $(python3 ~/setup/katapult/scripts/flash_can.py -q) )
 manta_uuid=( $(echo ${manta_uuid_querry[11]::-1}) )
 echo Manta UUID: $manta_uuid
-read -p "Connect Can via cable then press enter"
-echo "Querying Can UUID"
+read -p "Connect EBB Can via cable then press enter"
+echo "Querying EBB Can UUID"
 can_uuid_querry=( $(python3 ~/setup/katapult/scripts/flash_can.py -q) )
 can_uuid=( $(echo ${manta_uuid_querry[16]::-1}) )
 echo Manta UUID: $can_uuid
-echo -e "\e[1m\e[32m ----Setting manta uuid---- \e[0m"
+echo -e "\e[1m\e[32m ----Setting Manta uuid---- \e[0m"
 echo [mcu] >> /home/biqu/printer_data/config/printer.cfg
 echo canbus_uuid: $manta_uuid >> /home/biqu/printer_data/config/printer.cfg
 echo canbus_interface: can0 >> /home/biqu/printer_data/config/printer.cfg
-echo -e "\e[1m\e[32m ----Setting ebb can uuid---- \e[0m"
+echo -e "\e[1m\e[32m ----Setting EBB Can uuid---- \e[0m"
 echo [mcu EBBCan] >> /home/biqu/printer_data/config/can.cfg
 echo canbus_uuid: $can_uuid >> /home/biqu/printer_data/config/can.cfg
 echo canbus_interface: can0 >> /home/biqu/printer_data/config/can.cfg
+echo -e "\e[1m\e[32m ----Restarting Klipper---- \e[0m"
+sudo systemctl restart klipper
 echo -e "\e[1m\e[32m ----Everything done---- \e[0m"
 
